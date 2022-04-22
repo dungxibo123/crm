a709ca4d1ce7fa69f3bca75c348378c46d8deee1
dungxibo123 <votiendung0805@gmail.com>
2022-04-22 23:20:09 +0700

    update the html file for rendering information of customer

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('', views.home, name='home_'),
-    path('products/', views.products),
-    path('customer/', views.customer)
+    path('', views.home, name='home'),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk>/', views.customer, name="customer")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+from .models import *
 
 
 from django.http import HttpResponse
 # Create your views here.
 def home(r):
-    return render(r, './accounts/dashboard.html')
+    orders = Order.objects.all()
+    customer = Customer.objects.all()
+    total_customer = Customer.objects.all().count()
+    total_order = Order.objects.all().count()
+    delivered = Order.objects.filter(status='Delivered').count()
+    pending = Order.objects.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customer, 'total_customer': total_customer,
+               'total_order': total_order, 'delivered': delivered, 'pending': pending}
+    return render(r, './accounts/dashboard.html', context )
 def products(r):
-    return render(r, 'accounts/products.html')
-def customer(r):
-    return HttpResponse("Hihi")
+    products = Product.objects.all()
+    return render(r, 'accounts/products.html', {'products': products})
+def customer(r, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+    return render(r, 'accounts/customer.html', context)
 
